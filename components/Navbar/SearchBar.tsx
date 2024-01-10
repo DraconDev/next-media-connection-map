00e2a7302156f4acb05895daf405b0b7a91850ee
DraconDev <dracsharp@gmail.com>
2024-01-10 07:03:04 +0000

    upd nav

--- components/Navbar/SearchBar.tsx
+++ components/Navbar/SearchBar.tsx
@@ -10,14 +10,14 @@ const SearchBar = (props: Props) => {
     const [value, setValue] = useState("");
 
     return (
-        <div className="flex w-full grow outline-1 hover:outline-accent">
+        <div className="flex w-full grow hover:outline-1 outline-accent">
             <input
                 type="text"
-                className="w-full rounded-lg rounded-r-none bg-secondary outline-1 outline-accent px-3 py-1 grow flex"
+                className="w-full rounded-lg rounded-r-none bg-secondary outline-none px-3 py-1 grow flex"
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
             />
-            <Button override="bg-secondary rounded-lg rounded-l-none ">
+            <Button override="bg-secondary rounded-lg rounded-l-none">
                 <GrSearch className={"w-9 h-9"} />
             </Button>
         </div>
