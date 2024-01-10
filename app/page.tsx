edc4d34542eb241eceb622ab81dd38bc95c7efa2
DraconDev <dracsharp@gmail.com>
2024-01-10 05:16:45 +0000

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 export default function Home() {
-    return <main></main>;
+    return (
+        <main>
+            <Navbar />
+        </main>
+    );
 }
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+type Props = {};
+
+const Navbar = (props: Props) => {
+    return (
+        <div>
+            Navbar
+            <SearchBar />
+        </div>
+    );
+};
+
+export default Navbar;
