5a272890df189eb18c5819a13cd60ebbbf0da7ad
DraconDev <dracsharp@gmail.com>
2024-01-10 05:40:15 +0000

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
+import Button from "@/components/UI/Button";
 
 export default function Home() {
     return (
         <main>
             <Navbar />
+            <Button>Testing</Button>
             <Footer />
         </main>
     );
--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,7 +1,10 @@
-type Props = {};
+type Props = {
+    onClick?: () => void;
+    children?: JSX.Element | JSX.Element[] | string;
+};
 
 const Button = (props: Props) => {
-    return <div>Button</div>;
+    return <div className="p-2 bg-secondary">{props.children}</div>;
 };
 
 export default Button;
