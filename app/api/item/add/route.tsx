357faaf45f9cf614cd188327a97eaaf9cc6c8e71
DraconDev <dracsharp@gmail.com>
2024-01-10 07:26:30 +0000

    add post item

---
+++ app/api/item/add/route.tsx
@@ -0,0 +1,8 @@
+// next js post route
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    return NextResponse.json(body);
+}
