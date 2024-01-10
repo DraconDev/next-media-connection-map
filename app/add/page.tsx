0f40d05909fe195afdb07ac5a35e8728f758c1ac
DraconDev <dracsharp@gmail.com>
2024-01-10 08:49:20 +0000

    add form

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,14 +1,33 @@
+import { useForm } from "react-hook-form";
+
 type Props = {};
 
-const page = (props: Props) => {
+const AddItem = (props: Props) => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
     return (
         <div>
-            <form action="post">
-                <input type="text" />
-                <button type="submit">Add</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    defaultValue="title"
+                    {...(register("title"), { required: true })}
+                />
+                <input
+                    defaultValue="description"
+                    {...(register("description"), { required: true })}
+                />
+                <input
+                    defaultValue="image_url"
+                    {...(register("image_url"), { required: true })}
+                />
+                <button type="submit">Add item</button>
             </form>
         </div>
     );
 };
 
-export default page;
+export default AddItem;
