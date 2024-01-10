c7cb8fb32bb3b2dfd1ab8e09ec3e85c7ff8585fd
DraconDev <dracsharp@gmail.com>
2024-01-10 10:11:47 +0000

    form working

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -18,47 +18,52 @@ const AddItem = (props: Props) => {
 
     const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
     return (
-        <div className=" p-2 bg-red flex flex-col gap-3 justify-center items-center h-[80vh]">
+        <div className=" p-2 bg-red flex flex-col gap-3 justify-center items-center h-[80vh] w-full">
             <h1 className="text-4xl">Add an item</h1>
             <form
                 onSubmit={handleSubmit(onSubmit)}
                 className="flex flex-col gap-2 justify-center items-center"
             >
-                <div className="">
+                <div className="flex flex-col">
                     <p className="title p-1 text-2xl">Title</p>
                     <input
-                        defaultValue="title"
                         className="bg-primary p-2 rounded-md"
                         {...register("title", { required: true })}
                     />
+                    <p className="text-accent text-center">
+                        {errors.title && <span>Title is required</span>}
+                    </p>
                 </div>
-                <div className="">
+                <div className="flex flex-col">
                     <p className="description p-1 text-2xl">Description</p>
                     <input
-                        defaultValue="description"
                         className="bg-primary p-2 rounded-md"
                         {...register("description", { required: true })}
                     />
+                    <p className="text-accent text-center">
+                        {errors.description && (
+                            <span>Description is required</span>
+                        )}
+                    </p>
                 </div>
-                <div className="">
+                <div className="flex flex-col">
                     <p className="image_url p-1 text-2xl">Image url</p>
                     <input
-                        defaultValue="image_url"
                         className="bg-primary p-2 rounded-md"
                         {...register("image_url", { required: true })}
                     />
+                    <p className="text-accent text-center">
+                        {" "}
+                        {errors.image_url && <span>Image URL is required</span>}
+                    </p>
                 </div>
-                <button
+                <Button
                     type="submit"
-                    className="w-full bg-primary p-2"
+                    override="w-full"
                 >
                     Add item
-                </button>
-                <Button type="submit">Add item</Button>
+                </Button>
             </form>
-            {errors.title && <span>Title is required</span>}
-            {errors.description && <span>Description is required</span>}
-            {errors.image_url && <span>Image URL is required</span>}
         </div>
     );
 };
