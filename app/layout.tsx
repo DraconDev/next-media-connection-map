a7c88e5c61b6aa7ae770fbf19f526a5c93e63c9f
DraconDev <dracsharp@gmail.com>
2024-01-11 02:53:12 +0000

    upd nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,10 @@ export default function RootLayout({
             className="relative"
         >
             <body
-                className={`min-h-screen ${kanit.className}  text-font bg-secondary text-xl` }
+                className={`min-h-screen ${kanit.className}  text-font bg-secondary text-xl`}
             >
                 <Navbar />
-                <div className="w-full h-full pt-[130px] text-lg">
+                <div className="w-full h-full pt-[140px] text-lg p-1">
                     {children}
                 </div>
             </body>
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
     return (
-        <div className="p-4 fixed flex w-full bg-primary flex-col gap-4">
+        <div className="p-4 fixed flex w-full bg-primary flex-col gap-4 border-b-2 border-accent ">
             <FirstRow />
             <SecondRow />
         </div>
