d6e547130d24620070518160e84ecd35c158028c
DraconDev <dracsharp@gmail.com>
2024-01-11 04:12:45 +0000

    upd item

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Footer from "@/components/Footer/Footer";
 import Itemlist from "@/components/Itemlist/Itemlist";
-import Button from "@/components/UI/Button";
 import GetItems from "@/db/supabase";
 
 export default function Home() {
     GetItems();
     return (
         <main>
-            <Button>Testing</Button>
             <Itemlist />
             <Footer />
         </main>
--- components/Itemlist/Item.tsx
+++ components/Itemlist/Item.tsx
@@ -1,19 +1,23 @@
 import { ItemType } from "@/type/item";
 import Image from "next/image";
+import Link from "next/link";
 
 const Item = (item: ItemType) => {
     return (
-        <div>
-            Item
+        <Link
+            className="flex flex-col gap-2 bg-red"
+            href={`/item/${item.title.replace(" ", "-")}`}
+        >
             <Image
                 src={item.image_url ?? "/public/placeholder.jpg"}
                 alt={item.title}
-                className="w"
-                width={200}
-                height={200}
+                className="rounded-lg object-cover w-[200px] h-[200px] "
+                width={100}
+                height={180}
                 loader={() => item.image_url ?? "/public/placeholder.jpg"}
             ></Image>
-        </div>
+            <div className="p-1">{item.title}</div>
+        </Link>
     );
 };
 
