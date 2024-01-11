9907c8e7d29e493378a64ca84998a356046d1f7d
DraconDev <dracsharp@gmail.com>
2024-01-11 07:50:15 +0000

    upd fully working search

--- components/Navbar/SearchBar.tsx
+++ components/Navbar/SearchBar.tsx
@@ -4,21 +4,27 @@ import { useState } from "react";
 import { GrSearch } from "react-icons/gr";
 import Button from "../UI/Button";
 import GetItems, { SearchByTitle } from "@/db/supabase";
-import { useQuery } from "react-query";
-import { QueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "react-query";
+import { ItemType } from "@/type/item";
 
 type Props = {};
 
 const SearchBar = (props: Props) => {
     const [value, setValue] = useState("");
 
-    const {
-        isLoading,
-        error,
-        data: items,
-    } = useQuery(["items"], () => SearchByTitle(value), {
-        enabled: value !== "",
-    });
+    const queryClient = useQueryClient();
+
+    const updateItemsQueryData = (newData: ItemType[]) => {
+        queryClient.setQueryData(["items"], newData);
+    };
+
+    const handleSearch = async () => {
+        // Fetch new data using SearchByTitle
+        const newData = await SearchByTitle(value);
+        setValue("");
+        // Update the items query data in the cache
+        updateItemsQueryData(newData as ItemType[]);
+    };
 
     return (
         <div className="flex w-full grow hover:outline-1 outline-accent">
@@ -30,10 +36,7 @@ const SearchBar = (props: Props) => {
             />
             <Button
                 override="bg-secondary rounded-lg rounded-l-none"
-                action={() => {
-
-                    setValue("");
-                }}
+                action={handleSearch}
             >
                 <GrSearch className={"w-9 h-9"} />
             </Button>
