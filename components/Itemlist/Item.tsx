493afbb7fae39212962608e8136fb7ce8a0925e7
DraconDev <dracsharp@gmail.com>
2024-01-11 03:53:35 +0000

    add loader for images

--- components/Itemlist/Item.tsx
+++ components/Itemlist/Item.tsx
@@ -1,7 +1,20 @@
 import { ItemType } from "@/type/item";
+import Image from "next/image";
 
 const Item = (item: ItemType) => {
-    return <div>Item</div>;
+    return (
+        <div>
+            Item
+            <Image
+                src={item.image_url ?? "/public/placeholder.jpg"}
+                alt={item.title}
+                className="w"
+                width={200}
+                height={200}
+                loader={() => item.image_url ?? "/public/placeholder.jpg"}
+            ></Image>
+        </div>
+    );
 };
 
 export default Item;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        loader: "custom",
+        path: "",
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
