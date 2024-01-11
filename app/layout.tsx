ce0dab5ff25271988454482b245ef7e109e85ac3
DraconDev <dracsharp@gmail.com>
2024-01-11 03:02:15 +0000

    add query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar/Navbar";
+import QueryProvider from "@/components/ReactQuery/QueryProvider";
 import type { Metadata } from "next";
 import { Kanit } from "next/font/google";
 import "./globals.css";
@@ -20,14 +21,16 @@ export default function RootLayout({
             lang="en"
             className="relative"
         >
-            <body
-                className={`min-h-screen ${kanit.className}  text-font bg-secondary text-xl`}
-            >
-                <Navbar />
-                <div className="w-full h-full pt-[140px] text-lg p-1">
-                    {children}
-                </div>
-            </body>
+            <QueryProvider>
+                <body
+                    className={`min-h-screen ${kanit.className}  text-font bg-secondary text-xl`}
+                >
+                    <Navbar />
+                    <div className="w-full h-full pt-[140px] text-lg p-1">
+                        {children}
+                    </div>
+                </body>
+            </QueryProvider>
         </html>
     );
 }
