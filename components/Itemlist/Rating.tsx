8ed3afcd982e3116c7829dd34ef5d1af2ab7221b
DraconDev <dracsharp@gmail.com>
2024-01-13 05:45:44 +0000

    style rating

--- components/Itemlist/Rating.tsx
+++ components/Itemlist/Rating.tsx
@@ -1,39 +1,59 @@
 "use client";
-import { addDownvote, addUpvote } from "@/db/supabase";
+import { addDownvote } from "@/db/supabase";
 import { ItemType } from "@/type/item";
 import { FaRegThumbsDown, FaRegThumbsUp } from "react-icons/fa";
 import Button from "../UI/Button";
 
 const Rating = (item: ItemType) => {
     const ShareButtonStyle =
-        " rounded-full text-black w-full py-1 border-black border-2 ";
+        " rounded-full text-white w-full py-1 justify-center border-black border-2 ";
 
     return (
-        <div className="text-sm flex justify-center p-1 ">
-            <Button
-                override={
-                    "rounded-r-none bg-gradient-to-r from-tertiary to-accent border-r-0 via-tertiary" +
-                    ShareButtonStyle
-                }
-                action={() => addUpvote(item)}
-            >
-                <div className="flex justify-start px-2 gap-2 items-center">
-                    <FaRegThumbsUp className="w-7 h-7" />
-                    {item?.up_votes?.toString() ?? "0"}
-                </div>
-            </Button>
-            <Button
-                override={
-                    "rounded-l-none bg-gradient-to-r from-accent to-quaternary border-l-0 via-quaternary" +
-                    ShareButtonStyle
-                }
-                action={() => addDownvote(item)}
-            >
-                <div className="flex justify-end gap-2 items-center  px-2 ">
-                    <FaRegThumbsDown className="w-7 h-7" />
-                    {item?.down_votes?.toString() ?? "0"}
-                </div>
-            </Button>
+        <div className="">
+            <div className="text-sm flex justify-center p-1 ">
+                <Button override={"rounded-r-none " + ShareButtonStyle}>
+                    <div className="flex  px-2 gap-2 items-center justify-center">
+                        <FaRegThumbsUp className="w-7 h-7" />
+                        {item?.up_votes?.toString() ?? "0"}
+                    </div>
+                </Button>
+                <Button
+                    override={"rounded-l-none   " + ShareButtonStyle}
+                    action={() => addDownvote(item)}
+                >
+                    <div className="flex gap-2 items-center  justify-center px-2 ">
+                        <FaRegThumbsDown className="w-7 h-7" />
+                        {item?.down_votes?.toString() ?? "0"}
+                    </div>
+                </Button>
+            </div>
+            <div
+                className={`rounded-lg w-full h-2 bg-white px-1 bg-gradient-to-r from-tertiary  to-quaternary from-0% to-100%  ${
+                    item.up_votes / item.down_votes > 1
+                        ? `via-tertiary ${
+                              "via-" +
+                              (
+                                  (item.up_votes /
+                                      (item.down_votes + item.up_votes)) *
+                                  6.66
+                              )
+                                  .toFixed(0)
+                                  .toString()
+                                  .concat("0%")
+                          }`
+                        : `via-quaternary ${
+                              "via-" +
+                              (
+                                  (item.down_votes /
+                                      (item.down_votes + item.up_votes)) *
+                                  15
+                              )
+                                  .toFixed(0)
+                                  .toString()
+                                  .concat("0%")
+                          }`
+                }`}
+            ></div>
         </div>
     );
 };
--- type/item.ts
+++ type/item.ts
@@ -3,8 +3,8 @@ export type ItemType = {
     id: number;
     title: string;
     description?: string;
-    up_votes?: number;
-    down_votes?: number;
+    up_votes: number;
+    down_votes: number;
     image_url?: string;
     tags?: string[];
     connections?: string[];
