b8507abe3cb9907eca9e4768d6ee064a26970ef6
DraconDev <dracsharp@gmail.com>
2024-01-18 14:42:42 +0000

    add connections

--- components/Itemlist/OtherItems.tsx
+++ components/Itemlist/OtherItems.tsx
@@ -1,14 +1,28 @@
+import GetItems from "@/db/supabase";
+import { ItemType } from "@/type/item";
+import { useQuery } from "@tanstack/react-query";
 import { GoPlus } from "react-icons/go";
+import Item from "./Item";
 
 type Props = {};
 
 const OtherItems = () => {
+    const { data } = useQuery({ queryKey: ["connections"], queryFn: GetItems });
     return (
         <div className="bg-red-400">
             other card lists
             <div className="">
                 link card
                 <GoPlus />
+                <div className=" p-1 flex gap-2 flex-wrap justify-center">
+                    {data &&
+                        data.map((item: ItemType) => (
+                            <Item
+                                key={item.id}
+                                {...item}
+                            />
+                        ))}
+                </div>
             </div>
         </div>
     );
