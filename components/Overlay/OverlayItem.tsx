92696878ea1b2ec201f99a7ac29a1fe2a5fed278
DraconDev <dracsharp@gmail.com>
2024-01-18 17:44:15 +0000

    style link card

--- components/Overlay/OverlayItem.tsx
+++ components/Overlay/OverlayItem.tsx
@@ -1,26 +1,21 @@
 import { ItemType } from "@/type/item";
 import Image from "next/image";
-import Link from "next/link";
 import { MdOutlineAddLink } from "react-icons/md";
 
 const OverlayItem = (item: ItemType) => {
     return (
         <button className="hover:outline outline-4 outline-accent rounded-lg bg-black">
             <div className="relative">
-                <MdOutlineAddLink className="absolute w-14 h-14 bottom-8 right-1 text-accent bg-black/50 rounded-full" />
-                <Link href={`/item/${item.id}`}>
-                    <Image
-                        src={item.image_url ?? "/public/placeholder.jpg"}
-                        alt={item.title}
-                        className="rounded-lg object-cover md:w-[240px] md:h-[300px] w-[160px] h-[200px] grow rounded-b-none"
-                        width={700}
-                        height={700}
-                        loader={() =>
-                            item.image_url ?? "/public/placeholder.jpg"
-                        }
-                    ></Image>
-                </Link>
-                <div className="text-xl flex items-center px-1">
+                <MdOutlineAddLink className="absolute w-14 h-14 bottom-10 right-1 text-accent bg-black/50 rounded-full" />
+                <Image
+                    src={item.image_url ?? "/public/placeholder.jpg"}
+                    alt={item.title}
+                    className="rounded-lg object-cover md:w-[240px] md:h-[300px] w-[160px] h-[200px] grow rounded-b-none"
+                    width={700}
+                    height={700}
+                    loader={() => item.image_url ?? "/public/placeholder.jpg"}
+                ></Image>
+                <div className="text-xl flex items-center p-1">
                     {item.title.length > 30
                         ? item.title.slice(0, 30) + "..."
                         : item.title}
