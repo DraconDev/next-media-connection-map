e5e58fd0aadff5a1236a24189baeaf103c65fd91
DraconDev <dracsharp@gmail.com>
2024-01-18 16:24:01 +0000

    add overlay

--- components/Itemlist/ConnectedItems.tsx
+++ components/Itemlist/ConnectedItems.tsx
@@ -1,7 +1,8 @@
 import { GetConnections } from "@/db/supabase";
 import { ItemType } from "@/type/item";
 import { useQuery } from "@tanstack/react-query";
-import { MdOutlineAddLink } from "react-icons/md";
+import { useState } from "react";
+import Overlay from "../Overlay";
 import Item from "./Item";
 
 const ConnectedItems = (item: ItemType) => {
@@ -14,21 +15,23 @@ const ConnectedItems = (item: ItemType) => {
 
     console.log(data);
 
+    const [overlayToggle, setOverlayToggle] = useState(true);
+
     return (
-        <div className="flex">
-            <div className="">
-                <div className=" p-1 flex gap-2 flex-wrap justify-center">
-                    <button className="flex justify-center items-center text-accent bg-primary w-[160px] h-[280px] md:w-[240px] md:h-[380px] rounded-xl border-2 border-accent p-1">
-                        <MdOutlineAddLink className="w-20 h-20" />
-                    </button>
-                    {data &&
-                        data.map((item: ItemType) => (
-                            <Item
-                                key={item.id}
-                                {...item}
-                            />
-                        ))}
-                </div>
+        <div className="flex flex-col w-full justify-start">
+            {overlayToggle && <Overlay />}
+            <div className="flex justify-between text-xl px-1 w-full">
+                <div className="">Recommendations</div>
+                <div className="">+Add</div>
+            </div>
+            <div className=" p-1 flex gap-2 flex-wrap">
+                {data &&
+                    data.map((item: ItemType) => (
+                        <Item
+                            key={item.id}
+                            {...item}
+                        />
+                    ))}
             </div>
         </div>
     );
---
+++ components/Overlay.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const Overlay = (props: Props) => {
+    return (
+        <div className="fixed top-0 left-0 w-full h-full bg-black/50 z-50">
+            Overlay
+        </div>
+    );
+};
+
+export default Overlay;
