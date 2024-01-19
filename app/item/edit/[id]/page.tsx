085e8b8638a93a2c093e76ea4262f0e47e2ea55f
DraconDev <dracsharp@gmail.com>
2024-01-19 06:38:39 +0000

    upd working edit query

--- app/item/edit/[id]/page.tsx
+++ app/item/edit/[id]/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 import { GetItemById } from "@/db/supabase";
+import { ItemType } from "@/type/item";
 import { useQuery } from "@tanstack/react-query";
 
 const Edit = ({ params }: { params: { id: string } }) => {
-    const { data } = useQuery({
+    const { data, isLoading, error } = useQuery({
         queryKey: ["item"],
         queryFn: () => GetItemById(+params.id),
     });
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (error) {
+        return <div>Error</div>;
+    }
+    if (!data) {
+        return <div>Error</div>;
+    }
+    const item = data as ItemType;
+
+    console.log("item", item);
 
     return <div>{/* <UpdateForm item={props.item} /> */}</div>;
 };
