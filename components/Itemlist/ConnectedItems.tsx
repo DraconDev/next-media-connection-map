78b9d680f74931a45d9f531366fcb6d7ff05e2f7
DraconDev <dracsharp@gmail.com>
2024-01-19 06:17:09 +0000

    style add recommendation

--- components/Itemlist/ConnectedItems.tsx
+++ components/Itemlist/ConnectedItems.tsx
@@ -3,6 +3,7 @@ import { GetConnections } from "@/db/supabase";
 import { ItemType } from "@/type/item";
 import { useQuery } from "@tanstack/react-query";
 import { Dispatch, SetStateAction } from "react";
+import { GoPlus } from "react-icons/go";
 import Overlay from "../Overlay/Overlay";
 import Button from "../UI/Button";
 import Item from "./Item";
@@ -38,9 +39,14 @@ const ConnectedItems = ({
                     id={item.id}
                 />
             )}
-            <div className="flex justify-between text-xl px-1 w-full items-center">
+            <div className="flex justify-between text-2xl px-2 w-full items-center">
                 <div className="">Recommendations</div>
-                <Button action={() => setOverlayToggle(true)}>+Add</Button>
+                <Button
+                    action={() => setOverlayToggle(true)}
+                    override="bg-black text-accent rounded-full border-2 border-accent hover:bg-black  "
+                >
+                    <GoPlus className="w-9 h-9" />
+                </Button>
             </div>
             <div className=" p-1 flex gap-2 flex-wrap">
                 {data &&
