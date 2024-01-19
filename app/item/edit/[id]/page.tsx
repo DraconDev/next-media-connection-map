ac84b016aa4e9ae67aae32e224a9640a343ef518
DraconDev <dracsharp@gmail.com>
2024-01-19 06:33:08 +0000

    upd edit page

--- app/item/edit/[id]/page.tsx
+++ app/item/edit/[id]/page.tsx
@@ -1,7 +1,14 @@
-type Props = {};
+"use client";
+import { GetItemById } from "@/db/supabase";
+import { useQuery } from "@tanstack/react-query";
 
-const Edit = (props: Props) => {
-    return <div>Edit</div>;
+const Edit = ({ params }: { params: { id: string } }) => {
+    const { data } = useQuery({
+        queryKey: ["item"],
+        queryFn: () => GetItemById(+params.id),
+    });
+
+    return <div>{/* <UpdateForm item={props.item} /> */}</div>;
 };
 
 export default Edit;
